f7e159f9f3fa6c0497d67f6ef12785cbf0d98107
LiDong <lidong@xyhelper.cn>
2026-08-15 09:51:11 +0000

    update prod-75f4ed8e2569f3c558a452da206111f48ee26baf 20260815-175111

---
+++ cdn/assets/620167af-gy5qqxo291rcwp25.js
@@ -0,0 +1,2 @@
+import{n as e}from"./f025431a-ehagpvg3m4e1cduv.js";import{FO as t,RO as n}from"./4813494d-l4t5njeos7wg6tki.js";import{_n as r,gn as i,vn as a}from"./2340486e-ntgsg4a2ljrzv0ij.js";import{n as o,r as s}from"./b453068a-lz9p8iypelcxzs6g.js";function c({className:e=`h-10 w-10`}){let t=r(),{iconUrl:i}=o({isDarkMode:n()});return(0,l.jsx)(`img`,{src:i,alt:t.formatMessage({id:`codex-app.icon.alt`,defaultMessage:`Codex app icon`}),className:e,"aria-hidden":!0})}var l,u=e((()=>{t(),i(),s(),l=a()}));export{u as n,c as t};
+//# sourceMappingURL=620167af-gy5qqxo291rcwp25.js.map
